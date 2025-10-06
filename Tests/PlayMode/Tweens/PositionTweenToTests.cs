374c0fe859345c203a5a19fe33147b67eeb58467
Alex Bedard-Reid <alexbedardreid@gmail.com>
2025-10-05 21:12:26 -0700

    Tween Tests Implemented
    [ADD] Added separate files for each TweenTest (Position, Rotation, Scale)
    [MOD] Added custom Assertion to Rotation tests since Assert.Equals() was not working for Quaternion comparison

---
+++ Tests/PlayMode/Tweens/PositionTweenToTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Utilities.Tweening;
+using Object = UnityEngine.Object;
+
+namespace Tests.Tweens
+{
+    public class PositionTweenToTests
+    {
+        private Transform m_transform;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            m_transform = ObjectFactory.CreatePrimitive(PrimitiveType.Cube).transform;
+        }
+        [SetUp]
+        public void Setup()
+        {
+            m_transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
+        #region Test Position
+
+        [UnityTest]
+        public IEnumerator TweenPositionTests(
+            [Values(0f, 0.5f, 1f)] float time,
+            [Values(CURVE.LINEAR, CURVE.EASE_IN, CURVE.EASE_OUT, CURVE.EASE_IN_OUT)]
+            CURVE curve,
+            [ValueSource(nameof(TestTargetValues))]
+            Vector3 target)
+        {
+            bool hasCompleted = false;
+            try
+            {
+                if (time == 0f)
+                    LogAssert.Expect(LogType.Error, new Regex(".*Attempting to apply.*"));
+
+                m_transform.TweenTo(target, time, curve, () => { hasCompleted = true; });
+            }
+            catch (Exception e)
+            {
+                hasCompleted = true;
+                Console.WriteLine(e);
+                throw;
+            }
+
+            yield return new WaitUntil(() => hasCompleted);
+
+            Assert.AreEqual(m_transform.position, target);
+        }
+
+        [UnityTest]
+        public IEnumerator TweenLocalPositionTests(
+            [Values(0f, 0.5f, 1f)] float time,
+            [Values(CURVE.LINEAR, CURVE.EASE_IN, CURVE.EASE_OUT, CURVE.EASE_IN_OUT)]
+            CURVE curve,
+            [ValueSource(nameof(TestTargetValues))]
+            Vector3 target)
+        {
+            yield return null;
+            bool hasCompleted = false;
+            try
+            {
+                if (time == 0f)
+                    LogAssert.Expect(LogType.Error, new Regex(".*Attempting to apply.*"));
+
+                m_transform.TweenToLocal(target, time, curve, () => { hasCompleted = true; });
+            }
+            catch (Exception e)
+            {
+                hasCompleted = true;
+                Console.WriteLine(e);
+                throw;
+            }
+
+            yield return new WaitUntil(() => hasCompleted);
+
+            Assert.AreEqual(m_transform.localPosition, target);
+        }
+
+        #endregion
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(m_transform.gameObject);
+        }
+
+        public static IEnumerable<Vector3> TestTargetValues()
+        {
+            yield return Vector3.zero;
+            yield return Vector3.one;
+            yield return Vector3.one * -1f;
+        }
+    }
+}
---
+++ Tests/PlayMode/Tweens/RotationTweenToTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Utilities.Tweening;
+using Object = UnityEngine.Object;
+
+namespace Tests.Tweens
+{
+    public class RotationTweenToTests
+    {
+        private Transform m_transform;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            m_transform = ObjectFactory.CreatePrimitive(PrimitiveType.Cube).transform;
+        }
+        
+        [SetUp]
+        public void Setup()
+        {
+            m_transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+        }
+
+        #region Test Rotation
+
+        [UnityTest]
+        public IEnumerator TweenRotationTests(
+            [Values(0f, 0.5f, 1f)] float time,
+            [Values(CURVE.LINEAR, CURVE.EASE_IN, CURVE.EASE_OUT, CURVE.EASE_IN_OUT)]
+            CURVE curve,
+            [ValueSource(nameof(TestTargetRotationValues))]
+            Quaternion target)
+        {
+            yield return null;
+            bool hasCompleted = false;
+            try
+            {
+                if (time == 0f)
+                    LogAssert.Expect(LogType.Error, new Regex(".*Attempting to apply.*"));
+
+                m_transform.TweenTo(target, time, curve, () => { hasCompleted = true; });
+            }
+            catch (Exception e)
+            {
+                hasCompleted = true;
+                Console.WriteLine(e);
+                throw;
+            }
+
+            yield return new WaitUntil(() => hasCompleted);
+
+            CustomAssert(m_transform.rotation, target);
+        }
+
+        [UnityTest]
+        public IEnumerator TweenLocalRotationTests(
+            [Values(0f, 0.5f, 1f)] float time,
+            [Values(CURVE.LINEAR, CURVE.EASE_IN, CURVE.EASE_OUT, CURVE.EASE_IN_OUT)]
+            CURVE curve,
+            [ValueSource(nameof(TestTargetRotationValues))]
+            Quaternion target)
+        {
+            yield return null;
+            bool hasCompleted = false;
+            try
+            {
+                if (time == 0f)
+                    LogAssert.Expect(LogType.Error, new Regex(".*Attempting to apply.*"));
+
+                m_transform.TweenToLocal(target, time, curve, () => { hasCompleted = true; });
+            }
+            catch (Exception e)
+            {
+                hasCompleted = true;
+                Console.WriteLine(e);
+                throw;
+            }
+
+            yield return new WaitUntil(() => hasCompleted);
+
+            CustomAssert(m_transform.localRotation, target);
+        }
+
+        #endregion
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(m_transform.gameObject);
+        }
+
+        public static IEnumerable<Quaternion> TestTargetRotationValues()
+        {
+            yield return Quaternion.identity;
+            yield return Quaternion.Euler(90, 90, 90);
+            yield return Quaternion.Inverse(Quaternion.Euler(90, 90, 90));
+        }
+
+        private void CustomAssert(Quaternion a, Quaternion b)
+        {
+            const float tolerance = 0.001f;
+            
+            Assert.LessOrEqual(Quaternion.Angle(a,b), tolerance);
+        }
+    }
+}
