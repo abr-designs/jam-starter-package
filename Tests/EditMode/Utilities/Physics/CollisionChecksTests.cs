d41eaae07175e72749e044e19cdf26ad9eb2175c
Alex Bedard-Reid <alexbedardreid@gmail.com>
2025-10-14 20:17:53 -0700

    Update CollisionChecksTests.cs

--- Tests/EditMode/Utilities/Physics/CollisionChecksTests.cs
+++ Tests/EditMode/Utilities/Physics/CollisionChecksTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 using Utilities.Physics;
 
@@ -6,9 +7,13 @@ namespace Tests.Utilities.Physics
 {
     public class CollisionChecksTests
     {
-        public void Rect2Rect(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h) 
+        [Test]
+        [TestCase(-1,-1,1,1,1,1,1,1, ExpectedResult = false)]
+        [TestCase(-1,-1,2,2,1,1,2,2, ExpectedResult = true)]
+        [TestCase(0,0,1,1,0,0,2,2, ExpectedResult = true)]
+        public bool Rect2Rect(float r1x, float r1y, float r1w, float r1h, float r2x, float r2y, float r2w, float r2h)
         {
-            
+            return CollisionChecks.Rect2Rect(r1x, r1y, r1w, r1h, r2x, r2y, r2w, r2h);
         }
 
         [Test]
@@ -51,41 +56,71 @@ namespace Tests.Utilities.Physics
             return CollisionChecks.Line2Circle(x1,y1, x2, y2, cx, cy, r, out _, out _);
         }
         
-        public void Line2Point(float x1, float y1, float x2, float y2, float px, float py) 
+        [Test]
+        [TestCase(-1,0,1,0,0,0,ExpectedResult = true)]
+        [TestCase(-1,-1,-1,1,0,0,ExpectedResult = false)]
+        public bool Line2Point(float x1, float y1, float x2, float y2, float px, float py)
         {
-
-
+            return CollisionChecks.Line2Point(x1, y1, x2, y2, px, py);
         }
         
-        // LINE/RECTANGLE
-        public void Line2Rect(float x1, float y1, float x2, float y2, float rx, float ry, float rw, float rh) 
+        [Test]
+        [TestCase(-1,0, 1, 0, 0,0,1,1, ExpectedResult = true)]
+        [TestCase(-1,2, 1, 2, 0,0,1,1, ExpectedResult = false)]
+        [TestCase(-1,-1, 1, 1, 0,0,1,1, ExpectedResult = true)]
+        [TestCase(-1,0, 0, 0, 0,0,1,1, ExpectedResult = true)]
+        public bool Line2Rect(float x1, float y1, float x2, float y2, float rx, float ry, float rw, float rh) 
         {
-
+            return CollisionChecks.Line2Rect(x1, y1, x2, y2, rx, ry, rw, rh);
         }
-        public void Circle2Rect(float cx, float cy, float radius, float rx, float ry, float rw, float rh) 
+        [Test]
+        [TestCase(-2,0,1,0,0,1,1, ExpectedResult = false)]
+        [TestCase(-1,0,1,0,0,1,1, ExpectedResult = true)]
+        [TestCase(0,0,0.5f,0,0,1,1, ExpectedResult = true)]
+        [TestCase(-2,0,1,0,0,1,10, ExpectedResult = false)]
+        [TestCase(0,2,0.5f,0,0,10,1, ExpectedResult = false)]
+        public bool Circle2Rect(float cx, float cy, float radius, float rx, float ry, float rw, float rh)
         {
-
+            return CollisionChecks.Circle2Rect(cx, cy, radius, rx, ry, rw, rh);
         }
         
         // POLYGON/POINT
         public void Poly2Point(Vector3[] vertices, float px, float py)
         {
-
+            //TODO
         }
 
         // POLYGON/RECTANGLE
         public void Poly2Rect(Vector3[] vertices, float rx, float ry, float rw, float rh)
         {
-
+            //TODO
         }
 
         //============================================================================================================//
 
 
         //Based on: https://gdbooks.gitbooks.io/3dcollisions/content/Chapter1/closest_point_on_line.html
-        public void ClosestPointOnLine(Vector2 lineStart, Vector2 lineEnd, Vector2 point)
+        [Test]
+        [TestCaseSource(nameof(ClosestPointOnLineTestCaseData))]
+        public Vector2 ClosestPointOnLine(Vector2 lineStart, Vector2 lineEnd, Vector2 point)
         {
+            return CollisionChecks.ClosestPointOnLine(lineStart, lineEnd, point);
+        }
 
+        public static IEnumerable<TestCaseData> ClosestPointOnLineTestCaseData()
+        {
+            yield return new TestCaseData(Vector2.left, Vector2.right, Vector2.up)
+            {
+                ExpectedResult = Vector2.zero
+            };
+            yield return new TestCaseData(Vector2.left, Vector2.right, new Vector2(-1, 1))
+            {
+                ExpectedResult = Vector2.left
+            };
+            yield return new TestCaseData(Vector2.left, new Vector2(-1, -1), new Vector2(1, 0))
+            {
+                ExpectedResult = Vector2.left
+            };
         }
     }
 }
