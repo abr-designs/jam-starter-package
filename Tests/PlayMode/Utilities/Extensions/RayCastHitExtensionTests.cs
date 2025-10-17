5d675574182f440f1b4e949ce3a69408b134f1f2
Alex Bedard-Reid <32913470+abr-designs@users.noreply.github.com>
2025-10-17 08:11:17 -0700

    Feature/unit testing (#53)
    * Implement Test Framework Initial tests
    
    [ADD] Added CollectionExtensionTests.cs
    
    * Implement Edit Mode Tests
    
    [ADD] Added MathTests.cs
    
    * WIP Tween Testing
    
    * Tween Tests Implemented
    
    [ADD] Added separate files for each TweenTest (Position, Rotation, Scale)
    [MOD] Added custom Assertion to Rotation tests since Assert.Equals() was not working for Quaternion comparison
    
    * Create RaycastHitExtension tests
    
    * Move TweenTo tests
    
    * WIP Tests
    
    * Update CollisionChecksTests.cs
    
    * Update CHANGELOG.md

---
+++ Tests/PlayMode/Utilities/Extensions/RayCastHitExtensionTests.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Utilities;
+
+namespace Tests.Utilities.Extensions
+{
+    public class RayCastHitExtensionTests
+    {
+        private const int CUBE_COUNT = 10;
+        private const float CUBE_SPACING = 1.2f;
+        
+        private Transform[] cubes;
+        private Collider[] cubeColliders;
+        private Rigidbody[] cubeRigidbodies;
+
+        private RaycastHit[] m_raycastHits;
+        
+        [UnityOneTimeSetUp]
+        public IEnumerator OneTimeSetup()
+        {
+            m_raycastHits = new RaycastHit[CUBE_COUNT];
+            cubes = new Transform[CUBE_COUNT];
+            cubeColliders = new Collider[CUBE_COUNT];
+            cubeRigidbodies = new Rigidbody[CUBE_COUNT];
+            
+            for (int i = 0; i < CUBE_COUNT; i++)
+            {
+                var temp = ObjectFactory.CreatePrimitive(PrimitiveType.Cube);
+
+                temp.name = $"{nameof(RayCastHitExtensionTests)}_{nameof(PrimitiveType.Cube)}_[{i}]";
+                var collider = temp.GetComponent<BoxCollider>();
+                
+                var rb = temp.AddComponent<Rigidbody>();
+                rb.isKinematic = true;
+                
+                temp.transform.position = Vector3.forward * (i + 1) * CUBE_SPACING;
+
+                cubes[i] = temp.transform;
+                cubeColliders[i] = collider;
+                cubeRigidbodies[i] = rb;
+            }
+            
+            yield return null;
+        }
+
+        [TestCaseSource(nameof(NearestTestCases))]
+        public void GetNearestHitTest(Vector3 position, Vector3 direction, int expectedColliderIndex)
+        {
+            var raycastAll = Physics.RaycastAll(position, direction);
+            var count = raycastAll.Length;
+            
+            var furthestHit = raycastAll.GetNearestHit(count);
+            
+            if (expectedColliderIndex < 0)
+            {
+                Assert.True(furthestHit.Equals(default(RaycastHit)));
+                return;
+            }
+            
+            Assert.AreEqual(cubeColliders[expectedColliderIndex], furthestHit.collider);
+        }
+        
+        [TestCaseSource(nameof(FurthestTestCases))]
+        public void GetFurthestHitTest(Vector3 position, Vector3 direction, int expectedColliderIndex)
+        {
+            var raycastAll = Physics.RaycastAll(position, direction);
+            var count = raycastAll.Length;
+            
+            var furthestHit = raycastAll.GetFurthestHit(count);
+            
+            if (expectedColliderIndex < 0)
+            {
+                Assert.True(furthestHit.Equals(default(RaycastHit)));
+                return;
+            }
+            
+            Assert.AreEqual(cubeColliders[expectedColliderIndex], furthestHit.collider);
+        }
+        
+        [TestCaseSource(nameof(NearestTestCases))]
+        public void GetNearestHitNonAllocTest(Vector3 position, Vector3 direction, int expectedColliderIndex)
+        {
+            var count = Physics.RaycastNonAlloc(position, direction, m_raycastHits);
+            var furthestHit = m_raycastHits.GetNearestHit(count);
+            
+            if (expectedColliderIndex < 0)
+            {
+                Assert.True(furthestHit.Equals(default(RaycastHit)));
+                return;
+            }
+            
+            Assert.AreEqual(cubeColliders[expectedColliderIndex], furthestHit.collider);
+        }
+        
+        [TestCaseSource(nameof(FurthestTestCases))]
+        public void GetFurthestHitNonAllocTest(Vector3 position, Vector3 direction, int expectedColliderIndex)
+        {
+            var count = Physics.RaycastNonAlloc(position, direction, m_raycastHits);
+            var furthestHit = m_raycastHits.GetFurthestHit(count);
+
+            if (expectedColliderIndex < 0)
+            {
+                Assert.True(furthestHit.Equals(default(RaycastHit)));
+                return;
+            }
+            
+            Assert.AreEqual(cubeColliders[expectedColliderIndex], furthestHit.collider);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            for (int i = CUBE_COUNT - 1; i >= 0; i--)
+            {
+                Object.DestroyImmediate(cubes[i]);
+            }
+
+            cubes = null;
+            cubeColliders = null;
+            cubeRigidbodies = null;
+        }
+        
+        public static IEnumerable<TestCaseData> NearestTestCases()
+        {
+            yield return new TestCaseData(Vector3.zero, Vector3.forward, 0)
+                .SetName("Cast forward from origin, expect first");
+            yield return new TestCaseData(Vector3.forward * (CUBE_COUNT + 1) * CUBE_SPACING, Vector3.back, CUBE_COUNT - 1)
+                .SetName("Cast back from end, expect last");
+            yield return new TestCaseData(Vector3.zero, Vector3.back, -1)
+                .SetName("Cast back from origin, expect null");
+        }
+        public static IEnumerable<TestCaseData> FurthestTestCases()
+        {
+            yield return new TestCaseData(Vector3.zero, Vector3.forward, CUBE_COUNT - 1)
+                .SetName("Cast forward from origin, expect last");
+            yield return new TestCaseData(Vector3.forward * (CUBE_COUNT + 1) * CUBE_SPACING, Vector3.back, 0)
+                .SetName("Cast back from end, expect first");
+            yield return new TestCaseData(Vector3.zero, Vector3.back, -1)
+                .SetName("Cast back from origin, expect null");
+        }
+    }
+}
---
+++ Tests/PlayMode/Utilities/Extensions/Tweens/ScaleTweenToTests.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Utilities.Tweening;
+using Object = UnityEngine.Object;
+
+namespace Tests.Utilities.Extensions.Tweens
+{
+    public class ScaleTweenToTests
+    {
+        private Transform m_transform;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            m_transform = ObjectFactory.CreatePrimitive(PrimitiveType.Cube).transform;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            m_transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+            m_transform.localScale = Vector3.one;
+        }
+
+
+        #region Test Scale
+
+        [UnityTest]
+        public IEnumerator TweenScaleTests(
+            [Values(0f, 0.5f, 1f)] float time,
+            [Values(CURVE.LINEAR, CURVE.EASE_IN, CURVE.EASE_OUT, CURVE.EASE_IN_OUT)]
+            CURVE curve,
+            [ValueSource(nameof(TestTargetValues))]
+            Vector3 target)
+        {
+            bool hasCompleted = false;
+            try
+            {
+                if (time == 0f)
+                    LogAssert.Expect(LogType.Error, new Regex(".*Attempting to apply.*"));
+
+                m_transform.TweenScaleTo(target, time, curve, () => { hasCompleted = true; });
+            }
+            catch (Exception e)
+            {
+                hasCompleted = true;
+                Console.WriteLine(e);
+                throw;
+            }
+
+            yield return new WaitUntil(() => hasCompleted);
+
+            Assert.AreEqual(m_transform.localScale, target);
+        }
+
+        #endregion
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            if (m_transform == null)
+                return;
+
+            Object.Destroy(m_transform.gameObject);
+        }
+
+        public static IEnumerable<Vector3> TestTargetValues()
+        {
+            yield return Vector3.zero;
+            yield return Vector3.one;
+            yield return Vector3.one * -1f;
+        }
+    }
+
+}
