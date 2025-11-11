cbcc928c740692b01894a7306acd24000145130e
Alex Bedard-Reid <alexbedardreid@gmail.com>
2025-11-11 06:23:36 -0800

    Cleanup

--- Runtime/Scripts/FixedColorPaletteTool/FixedPaletteAttribute.cs
+++ Runtime/Scripts/FixedColorPaletteTool/FixedPaletteAttribute.cs
@@ -2,28 +2,6 @@
 
 namespace FixedColorPaletteTool
 {
-    /*public enum COLOR_TYPE
-    {
-        NONE = -1,
-        PRIMARY = 0,
-        SECONDARY = 1,
-        TERTIARY = 2
-    }*/
     public class FixedPaletteAttribute : PropertyAttribute
-    {
-        /*public int ColorIndex => (int)m_colorType;
-        private readonly COLOR_TYPE m_colorType;
-        
-        public FixedPaletteAttribute()
-        {
-            m_colorType = COLOR_TYPE.NONE;
-        }
-        
-        public FixedPaletteAttribute(COLOR_TYPE colorType)
-        {
-            m_colorType = colorType;
-        }*/
-        
-        
-    }
+    { }
 }
