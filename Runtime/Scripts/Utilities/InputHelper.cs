8294c8453843d95a2128b166d6628ef9d5f7f63c
Alex Bedard-Reid <alexbedardreid@gmail.com>
2025-12-06 05:28:41 -0800

    Added InputHelper.cs for axis input checks

---
+++ Runtime/Scripts/Utilities/InputHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public static class InputHelper
+    {
+        public static void AxisInput(KeyCode positive, KeyCode negative, ref float value)
+        {
+            if (UnityEngine.Input.GetKey(positive) || UnityEngine.Input.GetKey(negative))
+            {
+                if(UnityEngine.Input.GetKeyDown(positive))
+                    value = 1f;
+                if (UnityEngine.Input.GetKeyDown(negative))
+                    value = -1f;
+            }
+            else
+            {
+                value = 0f;
+            }
+        }
+    }
+    
+}
