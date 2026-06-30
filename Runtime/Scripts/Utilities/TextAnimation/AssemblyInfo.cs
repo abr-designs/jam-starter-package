401469fd25c1d301c4020dcf66e95f2564c93038
Alex Bedard-Reid <alexbedardreid@gmail.com>
2026-06-30 07:37:31 -0700

    More Attempted Fixes for Game.CI missing TMP Essentials

--- Runtime/Scripts/Utilities/TextAnimation/AssemblyInfo.cs
+++ Runtime/Scripts/Utilities/TextAnimation/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Jam-starter.Editor.TextAnimation")]
+[assembly: InternalsVisibleTo("Jam-starter.Runtime.Editor.TextAnimation")]
 [assembly: InternalsVisibleTo("com.abrds.jam-starter.Tests")]
